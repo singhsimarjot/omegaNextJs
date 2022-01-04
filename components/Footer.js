39a0ea283d7709d3b537290703bf4a2c3279cfec
singh_simarjot <simarjots9@gmail.com>
2022-01-04 15:55:51 +0530

    Linking Fix

--- components/Footer.js
+++ components/Footer.js
@@ -7,11 +7,11 @@ const Footer = () => {
                     <div
                         className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 ">
 
-
-                        <a href="/" className="d-flex align-items-center text-decoration-none text-white omega">
-                            Omega
-                        </a>
-
+                        <Link href="/">
+                            <a className="d-flex align-items-center text-decoration-none text-white omega">
+                                Omega
+                            </a>
+                        </Link>
                         <ul className="nav  justify-content-center mb-md-0 offwhite">
                             <li className="me-4 "><Link href="/support"><a className="nav-link px-2 link-secondary fs-6 offwhite">Support</a></Link></li>
                             <li className="me-4 "><Link href="/policy"><a className="nav-link px-2 link-dark fs-6 text-white-secondary">Privacy Policy
