bfbf16625d7f33957735856eeb24124469035331
singhsimarjot <simarjots9@gmail.com>
2022-03-14 18:02:57 +0530

    ui:"fixed the ui issue"

--- pages/about/index.js
+++ pages/about/index.js
@@ -18,7 +18,7 @@ const About = () => {
           rel="stylesheet"
           href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
           integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></link>
       </Head>
       <Header />
